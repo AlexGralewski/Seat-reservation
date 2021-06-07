6192c9b3f9fbdd4107a828a696cfc33489941b3d
AlexGralewski <gralewski.aleksander@gmail.com>
2021-06-07 16:05:53 +0200

    Initial commit

---
+++ src/components/StartingScreen.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import {Button} from 'antd'
+
+const StartingScreen = () => (
+  <div className='starting-screen'>
+    <Button type="primary">Button</Button>
+  </div>
+)
+
+export default StartingScreen
